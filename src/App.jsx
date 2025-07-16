8c78c741764a6567b3772fa1f78647292b6a2ef3
yeonjaegit <keo0713@naver.com>
2025-07-16 17:57:13 +0900

    state와 component에 대한 것

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,53 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Detail from './Detail';
 
 function App() {
-  let title = "게시판";
+  // const [변수명, 변경함수] = useState(초기값);
+  // useXXX : 리액트 내장함수 ( 리액트 훅 )
+  const [title, setTitle] = useState('게시판');
+  const [boardTitle, setBoardTitle] 
+  = useState(['React', 'HTML', 'CSS']);
+  const [like, setLike] = useState(0);
+  const [show, setshow] = useState(true);
+
+  function change() {
+    setLike(like + 1)
+  }
 
   return (
     <div className='App'>
       <div className='nav'>
         <h3>{title}</h3>
       </div>
+      <button onClick={() => {
+        setTitle('상품목록')
+      }}>제목바꾸기</button>
+      <div className='list'>
+        <h4>{boardTitle[0]}<button onClick={change}>좋아요</button> {like}</h4>
+        <p>2025-07-16</p>
+      </div>
+      <div className='list'>
+        <h4>{boardTitle[1]}</h4>
+        <p>2025-07-16</p>
+      </div>
+      <div className='list'>
+        <h4>{boardTitle[2]}</h4>
+        <p>2025-07-16</p>
+      </div>
+
+      <button onClick={() => {
+        let _boardTitle = [...boardTitle];
+        _boardTitle[0] = 'Java';
+
+        setBoardTitle(_boardTitle)
+      }}>첫 번째 게시물 제목바꾸기</button>
+
+      {
+        show ? <Detail /> : ''
+      }
+
     </div>
   )
 }
---
+++ src/Detail.jsx
@@ -0,0 +1,12 @@
+function Detail() {
+
+  return(
+    <div className='detail'>
+      <h4>글제목</h4>
+      <p>날짜</p>
+      <p>내용</p>
+    </div>
+  )
+}
+
+export default Detail;
